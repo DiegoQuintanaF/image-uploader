252af36fece68af968f7aa2c76cd808311a2aacf
Diego Quintana <dquintana@utb.edu.co>
2023-07-11 09:05:43 -0500

    update emviroments and added packages

--- src/config.js
+++ src/config.js
@@ -4,4 +4,10 @@ dotenv.config();
 
 export const PORT = process.env.PORT;
 
-export const DB_URL = process.env.DB_URL;
+export const DB_URI = process.env.DB_URI;
+
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
